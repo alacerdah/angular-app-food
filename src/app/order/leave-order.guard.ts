ce28de134931082ae23b60dfc1576968ce15c274
anna.lacerda <anna.lacerda@sigma4.com.br>
2021-03-15 22:28:09 -0300

    clean code pasta order e not-found

--- src/app/order/leave-order.guard.ts
+++ src/app/order/leave-order.guard.ts
@@ -1,14 +1,11 @@
 import { CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { OrderComponent } from "./order.component";
-import { OrderItemComponent } from "./order-item/order-item.component";
 
 export class LeaveOrderGuard implements CanDeactivate<OrderComponent> {
     constructor() {
 
     }
-    canDeactivate(orderComponent: OrderComponent,
-                  activatedRoute: ActivatedRouteSnapshot, 
-                  routerState: RouterStateSnapshot): boolean{
+    canDeactivate(orderComponent: OrderComponent): boolean{
         if(!orderComponent.isOrderCompleted()){
             return window.confirm('Deseja desistir da compra?')
         }else {
